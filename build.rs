925c023fde1d40f6cf7662eae49d354f3c5e81e1
Inflation <hypernovagama@gmail.com>
2020-08-14 21:46:10 -0400

    Use pkg-config to find packages

--- build.rs
+++ build.rs
@@ -6,22 +6,19 @@ fn main() {
     println!("cargo:rerun-if-changed=wrapper.h");
     println!("cargo:rerun-if-changed=wrapper.hpp");
 
-    let jpegxl_prefix = get_jpegxl_prefix();
-    let include_dir =
-        env::var("DEP_JPEGXL_INCLUDE").unwrap_or(format!("-I{}/include/jpegxl", jpegxl_prefix));
-    let lib_dir = env::var("DEP_JPEGXL_LIB").unwrap_or(format!("{}/lib", jpegxl_prefix));
+    let (include_dir, lib_dir) = get_jpegxl_dir();
     println!("cargo:rustc-link-lib=jpegxl");
     println!("cargo:rustc-link-search=native={}", lib_dir);
 
     let bindings = builder()
         .header("wrapper.h")
-        .clang_arg(&include_dir)
+        .clang_arg(format!("-I{}/jpegxl", &include_dir))
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
         .generate()
         .expect("Unable to generate bindings");
     let cppbindings = builder()
         .header("wrapper.hpp")
-        .clang_arg(&include_dir)
+        .clang_arg(format!("-I{}/jpegxl", &include_dir))
         .clang_arg("-xc++")
         .clang_arg("-std=c++17")
         .enable_cxx_namespaces()
@@ -41,12 +38,24 @@ fn main() {
 }
 
 #[cfg(not(feature = "build-jpegxl"))]
-fn get_jpegxl_prefix() -> String {
-    env::var("DEP_JPEGXL_PREFIX").unwrap_or("/usr/local/".to_string())
+fn get_jpegxl_dir() -> (String, String) {
+    let lib = pkg_config::Config::new().probe("libjpegxl").unwrap();
+    (
+        env::var("DEP_JPEGXL_INCLUDE")
+            .unwrap_or(lib.include_paths.first().unwrap().display().to_string()),
+        env::var("DEP_JPEGXL_LIB").unwrap_or(lib.link_paths.first().unwrap().display().to_string()),
+    )
 }
 
 #[cfg(feature = "build-jpegxl")]
-fn get_jpegxl_prefix() -> String {
-    use cmake::Config;
-    Config::new("jpeg-xl").build().display().to_string()
+fn get_jpegxl_dir() -> (String, String) {
+    let prefix = Config::new("jpeg-xl")
+        .build_target("jpegxl")
+        .build()
+        .display()
+        .to_string();
+    (
+        format!("{}/include/jpegxl", prefix),
+        format!("{}/lib", prefix),
+    )
 }
